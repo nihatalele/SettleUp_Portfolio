e56f85950fb0447346fb4cbb36e044c1afd4e165
dh1412 <kiddh1412@gmail.com>
2025-04-13 17:21:06 -0400

    added category selection for expenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_07_232628) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_13_211428) do
   create_table "expense_shares", force: :cascade do |t|
     t.integer "expense_id", null: false
     t.integer "participant_id", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_07_232628) do
     t.integer "trip_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category"
     t.index ["participant_id"], name: "index_expenses_on_participant_id"
     t.index ["trip_id"], name: "index_expenses_on_trip_id"
   end
@@ -38,6 +39,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_07_232628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_participants_on_trip_id"
+  end
+
+  create_table "test_enums", force: :cascade do |t|
+    t.integer "thing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trips", force: :cascade do |t|
